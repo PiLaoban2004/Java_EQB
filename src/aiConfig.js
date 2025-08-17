fdb2c3e4593015717fbdd0a14d795e608a9fef6a
PiLaoban2004 <481936898@qq.com>
2025-08-17 16:02:11 +0800

    解决ai服务bug

--- src/aiConfig.js
+++ src/aiConfig.js
@@ -1,3 +1,3 @@
 export const GEMINI_API_KEY = 'sk-XIPdNvlbZQeCzmdmRpMO1baVm4Ofps7yMrh5rOFuz0yoTKg-';
-export const GEMINI_API_ENDPOINT = 'http://47.99.123.168:3001/proxy/gemini';
+export const GEMINI_API_ENDPOINT = 'https://47.99.123.168:3001/proxy/gemini';
 export const GEMINI_MODEL = 'gemini-2.5-flash';
